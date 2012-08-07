06c3410663f7eda2d22a6541edcaad3f01507efd
Samuel Huberman <samuel.huberman@gmail.com>
2012-08-06 23:11:32 -0400

    added some basic parallelization

--- nmdtest.py
+++ nmdtest.py
@@ -1,7 +1,16 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import nmdsetup as nmdsetup
 import nmd as nmd
+import sys
+
+ppPath='/home/jason/lib/python'
+sys.path.append(ppPath)
+import pp
+
+job_server = pp.Server()
+ncpus=2
+job_server.set_ncpus(ncpus)
 
 eig=nmdsetup.Eig('eigvec.dat')
 vel=nmdsetup.Vel('dump.vel')
@@ -11,6 +20,7 @@ kpt=np.loadtxt('kptlist.dat')
 testnmd=nmd.Nmd(vel,pos,eig,kpt)
 
 #print testnmd.spctEnrg(1,1)
-plt.plot(np.real(testnmd.spctEnrg(1,1)))
-plt.show()
+job_server.submit(testnmd.spctEnrg,(1,1))
+#plt.plot(np.real(testnmd.spctEnrg(1,1)))
+#plt.show()
 
