37c3e0bed897c8905b43a0a65d182799860dfe38
Samuel Huberman <samuel.huberman@gmail.com>
2012-08-12 16:05:34 -0400

    added trivial parallelization

--- nmdpp.py
+++ nmdpp.py
@@ -2,7 +2,14 @@ import numpy as np
 #import matplotlib.pyplot as plt
 import nmdsetup as nmdsetup
 import sys
-import re
+
+ppPath='/home/jason/lib/python'
+sys.path.append(ppPath)
+import pp
+
+job_server = pp.Server()
+ncpus=2
+job_server.set_ncpus(ncpus)
 
 eig=nmdsetup.Eig('eigvec.dat')
 vel=nmdsetup.Vel('dump.vel')
@@ -11,9 +18,8 @@ kpt=np.loadtxt('kptlist.dat')
 
 testnmd=nmdsetup.Nmd(vel,pos,eig,kpt)
 
-testnmd.genscript(1)
-
 #print testnmd.spctEnrg(1,1)
-
-
+job_server.submit(testnmd.spctEnrg,(1,1))
+#plt.plot(np.real(testnmd.spctEnrg(1,1)))
+#plt.show()
 
