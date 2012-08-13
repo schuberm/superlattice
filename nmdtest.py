9a07290ba762d19514fc1b1190adb11c2dafd86e
Samuel Huberman <samuel.huberman@gmail.com>
2012-08-12 22:02:05 -0400

    templates

--- nmdtest.py
+++ nmdtest.py
@@ -7,11 +7,11 @@ import sys
 userin=ipf.InputParam()
 print userin.t_fft
 
-eig=nmdsetup.Eig('eigvec.dat')
+eig=nmdsetup.Eig('sampledata/eigvec.dat')
 print eig._file_name
-vel=nmdsetup.Vel('dump.vel')
-pos=nmdsetup.Pos('x0.dat')
-kpt=np.loadtxt('kptlist.dat')
+vel=nmdsetup.Vel('sampledata/dump.vel')
+pos=nmdsetup.Pos('sampledata/x0.dat')
+kpt=np.loadtxt('sampledata/kptlist.dat')
 
 testnmd=nmdsetup.Nmd(vel,pos,eig,kpt,userin)
 
