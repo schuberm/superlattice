642eca0a42d1f7e080327a0397c8587c49abe6b2
Samuel Huberman <samuel.huberman@gmail.com>
2012-10-15 21:53:26 -0400

    plots

--- imece12/plots/dvp/plotwvdk.m
+++ imece12/plots/dvp/plotwvdk.m
@@ -7,8 +7,8 @@ NMD.x0.LJ.tau = sqrt((NMD.x0.LJ.mass*(NMD.x0.LJ.sigma)^2)/NMD.x0.LJ.eps);
 kb = 1.3806E-23; 
 
 %period=[4,6,8,10,12]
-%period=[4,12,24]
-period=[4,12]
+period=[4,12,24]
+%period=[4,12]
 %colour=['b','c','g','m','r','k']
 colour=['b','r','g']
 
@@ -26,16 +26,16 @@ for i=1:1:length(period)
     hold on
     %semilogx(f(1:length(diff(cp))),diff(cp),colour(i),'LineWidth',2);
 
-    str_freq=strcat(num2str(period(i)),'p_freq_0.8.dat');
-    str_vel=strcat(num2str(period(i)),'p_vel_0.8.dat');
-    str_x0=strcat(num2str(period(i)),'p_x0_0.8.dat');
-    str_life=strcat(num2str(period(i)),'p_life_0.8.dat');
+    %str_freq=strcat(num2str(period(i)),'p_freq_0.8.dat');
+    %str_vel=strcat(num2str(period(i)),'p_vel_0.8.dat');
+    %str_x0=strcat(num2str(period(i)),'p_x0_0.8.dat');
+    %str_life=strcat(num2str(period(i)),'p_life_0.8.dat');
 
-    [f,ip,cp]=omegabin(str_freq,str_vel,str_life,str_x0);
+    %[f,ip,cp]=omegabin(str_freq,str_vel,str_life,str_x0);
     %semilogx(f(1:length(diff(cp,3))),diff(cp,3),colour(i),'LineWidth',1.5,'LineStyle','-.')
-    semilogx(f,smooth(cp),colour(i),'LineWidth',1.5,'LineStyle','-.')
+    %semilogx(f,smooth(cp),colour(i),'LineWidth',1.5,'LineStyle','-.')
     %trapz(smooth(cp))
-    ylim([0 0.06]) 
+    ylim([0 0.07]) 
     yL = get(gca,'YLim');
     line([period(i)*2*0.78161*NMD.x0.LJ.sigma period(i)*2*0.78161*NMD.x0.LJ.sigma],yL,'Color',colour(i));
 end
@@ -68,7 +68,7 @@ set(gca, ...
   'Position', [1 1 3.5 3.5]);
 
 set(gcf, 'PaperPositionMode', 'auto');
-print -depsc2 MFP_diffusion_contribution.eps
+print -depsc2 MFP_perfect_contribution.eps
 
 end
 
