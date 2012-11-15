8aaf11cbc3543d60f285775f757fc4973ca3e918
Samuel Huberman <samuel.huberman@gmail.com>
2012-11-15 18:29:38 -0500

    Final poster

--- ald_input/ald/m_ald_create_in.m
+++
@@ -1,7 +0,0 @@
-function m_ald_create_in(NMD)
-
-   NMD = m_ald_superlattice_stringchanges( NMD );
-    m_change_file_strings(...
-        NMD.ald.filename , NMD.ald.orig, strcat(NMD.gulp.str.main,'/',num2str(NMD.x0.superlattice.period(1,1)),'p_ald.in') , NMD.ald.change);
-
-end
