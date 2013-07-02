1f0e7093f1740fd267ef42fe21c1f4b03f85fc49
Samuel Huberman <samuel.huberman@gmail.com>
2013-07-02 15:34:39 -0400

    got rid of octave, py

--- code/nmdoctave/templates/spawn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $PBS_O_WORKDIR
-module load openmpi-psm-gcc
-
-RUNPATH=/home/jason/sam/20K/diffusion/4p_1066/perfect
-EXEPATH=/opt/mcgaugheygroup/matlab_R2011a/bin
-
-mpirun -np `cat $PBS_NODEFILE | wc -l` $EXEPATH/matlab -nojvm -nosplash -nodisplay -r -nodesktop < $RUNPATH/master.m
-
